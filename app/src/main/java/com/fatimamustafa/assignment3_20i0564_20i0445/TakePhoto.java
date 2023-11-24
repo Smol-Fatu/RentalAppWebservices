543f419a5a2651165b38caf593073f990c61d294
Fatima Mustafa <Smol-Fatu>
2023-11-24 22:16:48 +0500

    profile commit

---
+++ app/src/main/java/com/fatimamustafa/assignment3_20i0564_20i0445/TakePhoto.java
@@ -0,0 +1,14 @@
+package com.fatimamustafa.assignment3_20i0564_20i0445;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TakePhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_photo);
+    }
+}
